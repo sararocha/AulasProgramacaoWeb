77eedd8202f6af73d6a572377266d05149d411c3
sararocha <47834248+sararocha@users.noreply.github.com>
2019-06-07 21:57:44 -0300

    Finalização do CRUD
    Finalização do CRUD

---
+++ sistema/crud/controller/usuario/usuario.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+    require_once '../../view/head.php';
+    ?>
+</head>
+
+<body>
+    <?php
+    require_once '../../view/nav.php';
+    require_once 'usuarioDAO.php';
+    ?>
+    <div class="container">
+        <div class="row justify-content-center bg-light">
+            <form action="usuarioDAO.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $id ?>"
+                >
+                <div class="form-group">
+                    <label>Usuário</label>
+                    <input type="text" class="form-control" name="usuario" value="<?php echo $usuario ?>" id="usuario">
+                </div>
+                <div class="form-group">
+                    <label>Senha</label>
+                    <input type="password" class="form-control" name="senha" value="<?php echo $senha ?>" id="senha">
+                </div>
+
+                <div class="form-group">
+                 <?php if($id != 0) :?>
+                    <button type="submit" class="btn btn-info" name="atualizar">Atualizar</button>
+                 <?php else :?>
+                    <button type="submit" class="btn btn-primary" name="salvar">Salvar</button>
+<?php endif; ?>
+                </div>
+            </form>
+        </div>
+
+        <?php
+        require_once('../banco/conexao.php');
+
+        $resultado = $conexao->query("SELECT * FROM usuario") or die(mysqli_error($conexao));
+        ?>
+
+        <div class="row justufy-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Usuário</th>
+                        <th>Senha</th>
+                        <th colspan="3">Ações</th>
+                    </tr>
+                </thead>
+                <?php
+                while ($row = $resultado->fetch_assoc()) : ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['usuario']; ?></td>
+                        <td><?php echo $row['senha']; ?></td>
+                        <td>
+                            <a href="usuario.php?editar=<?php echo $row['id']; ?>" class="btn btn-info">Editar</a>
+                            <a href="usuarioDAO.php?excluir=<?php echo $row['id']; ?> " class="btn btn-danger">Excluir</a>
+
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+
+            </table>
+        </div>
+
+    </div>
+</body>
+
+</html>
